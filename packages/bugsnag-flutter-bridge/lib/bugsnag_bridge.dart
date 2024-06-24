728c4db3f95640003478606186272697287669e3
Robert <robert.smartbear@gmail.com>
2024-06-24 21:53:55 +0200

    Added bugsnag-flutter-bridge package

---
+++ packages/bugsnag-flutter-bridge/lib/bugsnag_bridge.dart
@@ -0,0 +1,3 @@
+export 'src/providers/http_headers_provider/http_headers_provider.dart';
+export 'src/providers/http_headers_provider/http_headers_provider_callbacks.dart'
+    show HttpHeadersProviderCallbacks;
